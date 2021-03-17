ede6447a3e9dde029c12d00bea825b084947935a
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-16 18:38:13 -0700

    intial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-Parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
+
+//TODO
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000")
+})
+
