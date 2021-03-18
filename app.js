3ec0a86aa11f47edc709eab76bd080039271c18b
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-17 18:12:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-Parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -9,6 +10,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 
+mongoose.connect('mongodb://localhost:27017/wikDB', {userNewUrlParser: true});
+
+const articaleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articaleSchema);
+    app
 //TODO
 
 app.listen(3000, function(){
