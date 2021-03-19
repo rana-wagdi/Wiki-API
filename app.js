a279afbd2b28d605439cf4f50d7009ac14b07630
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-18 21:23:17 -0700

    chained Route handlers

--- app.js
+++ app.js
@@ -19,34 +19,77 @@ const articaleSchema = {
 
 const Article = mongoose.model("Article", articaleSchema);
 
-app.get("/articles", function(req, res){
-    Article.find(function(err, foundArticles){
-        if (!err) {
-            res.send(foundArticles);
-        }else {
-            res.send(err)
-        }
-        
-    })
-})
-//postman:)
-app.post('/articles', function(req, res){
-  
-    
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
     const newArticle = new Article({
-        title:req.body.title,
-        content:req.body.content
+      title: req.body.title,
+      content: req.body.content,
     });
-    newArticle.save(function(err){
-        if (!err){
-         res.send("Successfully added a new article.")
-        }else {
-            res.send(err);
-        }
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article.");
+      } else {
+        res.send(err);
+      }
     });
-})
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully deleted all articles.");
+      } else {
+        res.send(err);
+      }
+    });
+  });
 
+app.get("/articles", )
+//postman:)
+app.post('/articles', )
 
+app.delete("/articles", );
+// [
+//   ({
+//     _id: "60515565187eb9938097364b",
+//     title: "REST",
+//     content:
+//       "REST is short for REpresentational State Transfer. It's an architectural style for desinging APIs",
+//   },
+//   {
+//     _id: "5c139771d79ac8eac11e754a",
+//     title: "API",
+//     content:
+//       "API stands for Application Programming Interface. It is a set of subroutine definitions, communication protocols, and tools for building software. In general terms, it is a set of clearly defined methods of communication among various components. A good API makes it easier to develop a computer program by providing all the building blocks, which are then put together by the programmer.",
+//   },
+//   {
+//     _id: "5c1398aad79ac8eac11e7561",
+//     title: "Bootstrap",
+//     content:
+//       "This is a framework developed by Twitter that contains pre-made front-end templates for web design",
+//   },
+//   {
+//     _id: "5c1398ecd79ac8eac11e7567",
+//     title: "DOM",
+//     content:
+//       "The Document Object Model is like an API for interacting with our HTML",
+//   },
+//   {
+//     _id: "60540194385162199477d1f4",
+//     title: "jack baur",
+//     content: "tack bar quick",
+//     __v: 0,
+//   })
+// ];
 //TODO
 
 app.listen(3000, function(){
